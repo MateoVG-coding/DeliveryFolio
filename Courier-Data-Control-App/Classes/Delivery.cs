98eca4d9bd947112599b4399c39ff5e56b737fcb
MateoVG-coding <mateovega10@hotmail.com>
2024-05-14 10:20:46 -0400

    Add status attribute to Delivery class.

--- Courier-Data-Control-App/Classes/Delivery.cs
+++ Courier-Data-Control-App/Classes/Delivery.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -15,14 +16,16 @@ namespace Courier_Data_Control_App.Classes
         private int PhoneNumber;
         private string Address;
         private string Description;
+        private bool Status;
         private Driver Driver;
 
-        public Delivery(string name, int phoneNumber, string address, string description, Driver driver = null)
+        public Delivery(string name, int phoneNumber, string address, string description, bool status = false, Driver driver = null)
         {
             Name = name;
             PhoneNumber = phoneNumber;
             Address = address;
             Description = description;
+            Status = status;
             Driver = driver;
         }
     }

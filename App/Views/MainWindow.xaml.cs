cd15cf8b86456b96c8b634f41e5d108086b1f9ec
MateoVG-coding <mateovega10@hotmail.com>
2024-07-23 21:53:25 -0400

    Add main window of UI.

--- App/Views/MainWindow.xaml.cs
+++ App/Views/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace Courier_Data_Control_App
         {
             InitializeComponent();
         }
+
+        // Start: Button Close | Restore | Minimize 
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void RestoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // End: Button Close | Restore | Minimize
     }
 }
